e188bfecb6d2eae388edee3ac71ce2be6c8f7dab
Nicolai <dalaaker.nicolai@gmail.com>
2023-02-21 11:48:58 +0100

    adjust piece rotation on letting go

--- MRTK2-Master/Assets/chess/scripts/ChessPieceScript.cs
+++ MRTK2-Master/Assets/chess/scripts/ChessPieceScript.cs
@@ -14,6 +14,9 @@ public class ChessPieceScript : MonoBehaviour
     public void setYPosToBoardPos(){
         //set local y to 0, which should be the starting y value. 
         this.transform.localPosition = new Vector3(transform.localPosition.x, 0, transform.localPosition.z);
+
+        //decided to adjust the position when the piece is let go, so that hte player can twirl it. If this feels weird, then a constraint should be added to the piee so that it is always upright, even in movement.
+        this.transform.localRotation = Quaternion.identity;
     }
 
 }
