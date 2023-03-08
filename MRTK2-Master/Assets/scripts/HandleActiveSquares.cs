4e98f72a9def9e3295359933dc0d328e7e232ec6
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2023-03-08 13:48:30 +0100

    removed unused variable

--- MRTK2-Master/Assets/scripts/HandleActiveSquares.cs
+++ MRTK2-Master/Assets/scripts/HandleActiveSquares.cs
@@ -7,77 +7,84 @@ public class HandleActiveSquares : MonoBehaviour
 
     public static GameObject startSquare;
     public static GameObject targetSquare;
-    bool isMoveActive = false;
     public float speed = .2f;
-     
+
     void Update()
     {
 
-       
-       if (startSquare != null && targetSquare != null) {
-        GameObject piece = FindClosestPieceOnStartSquare(startSquare);
-        if( piece.transform.position != targetSquare.transform.position) {
-             movePiece(piece, targetSquare);
-        }
-        else {
+
+        if (startSquare != null && targetSquare != null)
+        {
+            GameObject piece = FindClosestPieceOnStartSquare(startSquare);
+            if (piece.transform.position != targetSquare.transform.position)
+            {
+                movePiece(piece, targetSquare);
+            }
+            else
+            {
                 piece.transform.parent = targetSquare.transform;
                 startSquare.GetComponent<Renderer>().enabled = false;
                 targetSquare.GetComponent<Renderer>().enabled = false;
                 startSquare = null;
                 targetSquare = null;
             }
-       
+
         }
     }
 
-   
-   public void movePiece(GameObject currentObject, GameObject targetSquare) {
-    float step = speed * Time.deltaTime;
-    Vector3 targetPos = targetSquare.transform.position;
-    // Debug.Log(currentObject.name + " is closest");
-    currentObject.transform.position = Vector3.MoveTowards(currentObject.transform.position, targetPos, step);
+
+    public void movePiece(GameObject currentObject, GameObject targetSquare)
+    {
+        float step = speed * Time.deltaTime;
+        Vector3 targetPos = targetSquare.transform.position;
+        // Debug.Log(currentObject.name + " is closest");
+        currentObject.transform.position = Vector3.MoveTowards(currentObject.transform.position, targetPos, step);
         checkIfPieceCaptured(currentObject);
 
     }
 
 
-    public void checkIfPieceCaptured(GameObject currentObject) {
+    public void checkIfPieceCaptured(GameObject currentObject)
+    {
         GameObject[] pieces = GameObject.FindGameObjectsWithTag("piece");
-        foreach(GameObject piece in pieces) {
-            if(piece.transform.position == currentObject.transform.position && piece != currentObject) {
+        foreach (GameObject piece in pieces)
+        {
+            if (piece.transform.position == currentObject.transform.position && piece != currentObject)
+            {
                 piece.SetActive(false);
             }
         }
     }
 
-    public static bool SquareContainsPiece(GameObject square) {
+    public static bool SquareContainsPiece(GameObject square)
+    {
         return square.transform.childCount > 0;
     }
 
 
 
-public static GameObject FindClosestPieceOnStartSquare(GameObject startSquare)
-{
-    GameObject[] piecesOnStartSquare = GameObject.FindGameObjectsWithTag("piece");
-    float closestDistance = Mathf.Infinity;
-    GameObject closestPiece = null;
-
-    foreach (GameObject piece in piecesOnStartSquare)
+    public static GameObject FindClosestPieceOnStartSquare(GameObject startSquare)
     {
-        if (piece.transform.parent.gameObject == startSquare)
+        GameObject[] piecesOnStartSquare = GameObject.FindGameObjectsWithTag("piece");
+        float closestDistance = Mathf.Infinity;
+        GameObject closestPiece = null;
+
+        foreach (GameObject piece in piecesOnStartSquare)
         {
-            float distance = Vector3.Distance(startSquare.transform.position, piece.transform.position);
-            if (distance < closestDistance)
+            if (piece.transform.parent.gameObject == startSquare)
             {
-                closestDistance = distance;
-                closestPiece = piece;
+                float distance = Vector3.Distance(startSquare.transform.position, piece.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestPiece = piece;
+                }
             }
         }
+
+        return closestPiece;
     }
-
-    return closestPiece;
-}
 }
 
-      
+
 
