3db118bf2a21f62b0a980bb430a293474d894951
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2023-03-08 13:31:42 +0100

    refactoring

--- MRTK2-Master/Assets/BoardCreation/BoardFactory.cs
+++ MRTK2-Master/Assets/BoardCreation/BoardFactory.cs
@@ -6,31 +6,36 @@ using Unity.Netcode;
 
 public class BoardFactory : NetworkBehaviour
 {
-  
+
     [SerializeField] private GameObject boardNonPhysical;
     private bool physicalBoard = true;
 
-    public void changeBoard() {
-        
+    public void changeBoard()
+    {
+
         physicalBoard = !physicalBoard;
         Debug.Log("Physical board: " + physicalBoard);
     }
 
 
     [SerializeField]
- 
+
     private GameObject boardPrefab, wrapper, player;
 
-    public  void InstantiateBoardBasedOnCorners(Vector3 pA, Vector3 pC){
+    public void InstantiateBoardBasedOnCorners(Vector3 pA, Vector3 pC)
+    {
         setTransformBasedOn2Corners(wrapper, pA, pC);
 
-        if(IsServer){
+        if (IsServer)
+        {
             GameObject newBoard = (physicalBoard ? Instantiate(boardPrefab) : Instantiate(boardNonPhysical));
-             newBoard.GetComponent<NetworkObject>().Spawn();
+            newBoard.GetComponent<NetworkObject>().Spawn();
             newBoard.GetComponent<NetworkObject>().TrySetParent(wrapper);
             RescaleWrapperContentClientRpc();
-         
-        }else{
+
+        }
+        else
+        {
             RescaleWrapperContent();
         }
 
@@ -38,35 +43,40 @@ public class BoardFactory : NetworkBehaviour
     }
 
 
-    private void setTransformBasedOn2Corners(GameObject board ,Vector3 pA, Vector3 pC){
+    private void setTransformBasedOn2Corners(GameObject board, Vector3 pA, Vector3 pC)
+    {
         //ignores changes in y plane
         //Assumes a square board, as it is used in chess and lessens setup time
 
-        Vector3 crossSection = new Vector3(pC.x-pA.x, 0, pC.z-pA.z);
+        Vector3 crossSection = new Vector3(pC.x - pA.x, 0, pC.z - pA.z);
         Vector3 perpendicularCrossSection = new Vector3(crossSection.z, 0, -crossSection.x);
-        float  crossSectionMagnitude = Vector3.Magnitude(crossSection);
-        Vector3 center = pA + crossSection/2;
-        Vector3 pB = center + perpendicularCrossSection/2;
+        float crossSectionMagnitude = Vector3.Magnitude(crossSection);
+        Vector3 center = pA + crossSection / 2;
+        Vector3 pB = center + perpendicularCrossSection / 2;
 
         float width = Vector3.Distance(pA, pB);
-        
+
         board.transform.localScale = new Vector3(width, width, width);
-        
-        Quaternion rotation = Quaternion.LookRotation((pA + (pB-pA)/2) - center, Vector3.up);
-        board.transform.SetPositionAndRotation(center , rotation);
+
+        Quaternion rotation = Quaternion.LookRotation((pA + (pB - pA) / 2) - center, Vector3.up);
+        board.transform.SetPositionAndRotation(center, rotation);
     }
 
 
     [ClientRpc]
-    private void RescaleWrapperContentClientRpc(){
+    private void RescaleWrapperContentClientRpc()
+    {
         RescaleWrapperContent();
     }
 
-    private void RescaleWrapperContent(){
+    private void RescaleWrapperContent()
+    {
         //resets all the children within the wrapper, used to make sure that it properly comforms 
-        for(int i = 0; i < wrapper.transform.childCount; i++){
+        for (int i = 0; i < wrapper.transform.childCount; i++)
+        {
             var child = wrapper.transform.GetChild(i);
-            if(child != null){
+            if (child != null)
+            {
                 child.localPosition = Vector3.zero;
                 child.localScale = Vector3.one;
                 child.localRotation = Quaternion.identity;
@@ -74,13 +84,14 @@ public class BoardFactory : NetworkBehaviour
         }
     }
 
-    private void transformScaleWrapperAndPlayerToWorldCenter(){
+    private void transformScaleWrapperAndPlayerToWorldCenter()
+    {
         //moves the board to position 0,0,0 , with rotation of 0 so that it can be synchronized with the other players.
         //also moves the player the boared using parenting
-        var previousParent =  player.transform.parent;
+        var previousParent = player.transform.parent;
         player.transform.SetParent(wrapper.transform);
         wrapper.transform.position = Vector3.zero;
         wrapper.transform.rotation = Quaternion.identity;
-        player.transform.SetParent(previousParent);   
+        player.transform.SetParent(previousParent);
     }
 }
--- MRTK2-Master/Assets/SquareHandler.cs
+++ MRTK2-Master/Assets/SquareHandler.cs
@@ -4,7 +4,7 @@ using Microsoft.MixedReality.Toolkit.Input;
 using Unity.Netcode;
 using UnityEngine;
 
-public class SquareHandler : MonoBehaviour,IMixedRealityPointerHandler
+public class SquareHandler : MonoBehaviour, IMixedRealityPointerHandler
 {
     private bool active = false;
     [SerializeField] private GameObject networkObjectParent;
@@ -13,46 +13,49 @@ public class SquareHandler : MonoBehaviour,IMixedRealityPointerHandler
     {
 
         Debug.Log(eventData.selectedObject);
-        if(HandleActiveSquares.startSquare == null && HandleActiveSquares.SquareContainsPiece(gameObject)) {
+        if (HandleActiveSquares.startSquare == null && HandleActiveSquares.SquareContainsPiece(gameObject))
+        {
             RequestOwnership();
             Debug.Log("active square clicked");
             HandleActiveSquares.startSquare = gameObject;
             HandleActiveSquares.startSquare.GetComponent<Renderer>().enabled = true;
             HandleActiveSquares.startSquare.GetComponent<Renderer>().material.color = new Color(200, 0, 0, 1);
         }
-        else if (HandleActiveSquares.targetSquare == null && HandleActiveSquares.startSquare != null){
+        else if (HandleActiveSquares.targetSquare == null && HandleActiveSquares.startSquare != null)
+        {
             Debug.Log("target square clicked");
             HandleActiveSquares.targetSquare = gameObject;
             HandleActiveSquares.targetSquare.GetComponent<Renderer>().enabled = true;
             HandleActiveSquares.targetSquare.GetComponent<Renderer>().material.color = new Color(0, 0, 200, 1);
 
-           
+
         }
 
-      
-       
+
+
         active = true;
-        
+
     }
 
 
-    public void RequestOwnership(){
+    public void RequestOwnership()
+    {
         OwnershipManager.Instance.RequestOwnershipOfObject(networkObjectParent);
     }
-    
+
 
     public void OnPointerDragged(MixedRealityPointerEventData eventData)
     {
-       
+
     }
 
     public void OnPointerUp(MixedRealityPointerEventData eventData)
     {
-     
+
     }
 
     public void OnPointerClicked(MixedRealityPointerEventData eventData)
     {
-       
+
     }
 }
