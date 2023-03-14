32f95a0e7ea54919c2d5b84ef1fdbb3cfb4d4ad9
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2023-03-14 12:32:49 +0100

    added toggle avatar function (not tested)

---
+++ MRTK2-Master/Assets/optionPanel.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEditor;
+using UnityEngine;
+
+public class optionPanel : NetworkBehaviour
+{
+
+
+    // default values
+    bool renderAvatar = true;
+    bool physicalBoard = true;
+    bool renderBoard = true;
+    bool isWhite = true;
+
+
+    private SerializeField[] boardPhysical;
+    private SerializeField[] touchBoard;
+
+    public static GameObject activeBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // disables player prefab rendering
+    public void toggleAvatarOnOff()
+    {
+
+        // Get all connected clients
+        foreach (var networkClient in NetworkManager.Singleton.ConnectedClientsList)
+        {
+            // Check if the network object belongs to another player
+            if (!networkClient.PlayerObject.GetComponent<NetworkObject>().IsLocalPlayer)
+            {
+                // Disable the mesh renderer component
+                Renderer renderer = networkClient.PlayerObject.GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    Debug.Log("disabled avatar renderer");
+                    renderer.enabled = false;
+                }
+                else
+                {
+                    Debug.Log("enabled avatar renderer");
+                    renderer.enabled = true;
+                }
+            }
+        }
+    }
+
+    public void toggleBoardOnOff()
+    {
+        Debug.Log("board toggler clicked");
+        //BoardFactory.changeBoard();
+        List<GameObject> objectsInScene = GetNonSceneObjects();
+        GameObject board = null;
+        foreach (GameObject o in objectsInScene)
+        {
+            if (o.tag == "Board")
+            {
+                Debug.Log("found board");
+                board = o;
+                Debug.Log(board.name);
+                if (board.activeSelf)
+                {
+                    board.SetActive(false);
+                    Debug.Log("board disabled");
+                }
+                else
+                {
+                    Debug.Log("board enabled");
+                    board.SetActive(true);
+                }
+
+            }
+
+        }
+
+
+    }
+
+    List<GameObject> GetNonSceneObjects()
+    {
+        List<GameObject> objectsInScene = new List<GameObject>();
+
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (EditorUtility.IsPersistent(go.transform.root.gameObject) && !(go.hideFlags == HideFlags.NotEditable || go.hideFlags == HideFlags.HideAndDontSave))
+                objectsInScene.Add(go);
+        }
+
+        return objectsInScene;
+    }
+
+    public void changePlayerColor()
+    {
+        GameObject board = GameObject.FindGameObjectWithTag("Board");
+        board.transform.Rotate(0, 180, 0);
+    }
+
+    public void changeBoardType()
+    {
+
+    }
+
+
+
+
+}
