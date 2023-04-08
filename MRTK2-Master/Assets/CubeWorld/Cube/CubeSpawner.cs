9c7740a1a12843df061ca6436fb3993d86a0d0fd
Nicolai <dalaaker.nicolai@gmail.com>
2023-04-08 14:34:57 +0200

    configured for hololens, added 2.5cm cube

--- MRTK2-Master/Assets/CubeWorld/Cube/CubeSpawner.cs
+++ MRTK2-Master/Assets/CubeWorld/Cube/CubeSpawner.cs
@@ -13,7 +13,7 @@ public class CubeSpawner : MonoBehaviour
     [SerializeField, Range(1, 10)] //change in editor only when debuging
     private int currentSelectedCubeIndex = 0;
     private List<GameObject> instantiatedCubes = new List<GameObject>();
-    private static float[] CUBE_SIZES = {0.01f, 0.015f, 0.02f, 0.03f, 0.04f, 0.05f, 0.06f, 0.07f, 0.08f, 0.09f, 0.10f};
+    private static float[] CUBE_SIZES = {0.01f, 0.015f, 0.02f, 0.025f, 0.03f, 0.04f, 0.05f, 0.06f, 0.07f, 0.08f, 0.09f, 0.10f};
     
 
     [SerializeField] private DistanceMeasurer measurePoint;
